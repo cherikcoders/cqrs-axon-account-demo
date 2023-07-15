c57436b74f4c5f986852fdb2083f4f3bcc4fd264
mohamad-mahani <ms.mohamad15@gmail.com>
2023-07-15 17:05:54 +0330

    first commit

---
+++ src/main/java/com/example/axoncqrsbankingdemo/AxonCqrsBankingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.axoncqrsbankingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonCqrsBankingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonCqrsBankingDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/axoncqrsbankingdemo/command/service/AccountCommandService.java
@@ -0,0 +1,44 @@
+package com.example.axoncqrsbankingdemo.command.service;
+
+import com.example.axoncqrsbankingdemo.command.command.CreateAccountCommand;
+import com.example.axoncqrsbankingdemo.command.command.DepositMoneyCommand;
+import com.example.axoncqrsbankingdemo.command.command.WithdrawMoneyCommand;
+import com.example.axoncqrsbankingdemo.command.dto.CreateAccountRequest;
+import com.example.axoncqrsbankingdemo.command.dto.DepositRequest;
+import com.example.axoncqrsbankingdemo.command.dto.WithdrawalRequest;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class AccountCommandService {
+
+    private final CommandGateway commandGateway;
+
+    public AccountCommandService(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    public CompletableFuture<String> createAccount(CreateAccountRequest createAccountRequest) {
+        return commandGateway.send(new CreateAccountCommand(
+                UUID.randomUUID().toString(),
+                createAccountRequest.getStartingBalance())
+        );
+    }
+
+    public CompletableFuture<String> depositToAccount(DepositRequest depositRequest) {
+        return commandGateway.send(new DepositMoneyCommand(
+                depositRequest.getAccountId(),
+                depositRequest.getAmount()
+        ));
+    }
+
+    public CompletableFuture<String> withdrawFromAccount(WithdrawalRequest withdrawalRequest) {
+        return commandGateway.send(new WithdrawMoneyCommand(
+                withdrawalRequest.getAccountId(),
+                withdrawalRequest.getAmount()
+        ));
+    }
+}
